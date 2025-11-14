c6a111410b6b24772b34ba20f69f7755998e6381
theking90000 <martin.cogh@gmail.com>
2025-11-14 16:10:56 +0100

    Modification script run

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-podman run --rm -it --name phpfpm --net app -p 3001:80 -v ./src:/var/www/html -e "WEBROOT=/var/www/html/public/" -v ./src/nginx.conf:/etc/nginx/sites-enabled/default.conf:ro richarvey/nginx-php-fpm:latest
+# Pour avoir la config auto:
+# Créer src/phpmyadmi.php
+# avec 
+# <?php
+# $cfg['Servers'][1]['auth_type'] = 'config';
+# $cfg['Servers'][1]['host'] = '...';
+# $cfg['Servers'][1]['user'] = '...';
+# cfg['Servers'][1]['password'] = '...';
+
+docker run --rm -it --name phpmyadmin --net app -p 8080:80 -v ./src/phpmyadmin.php:/etc/phpmyadmin/config.user.inc.php:ro phpmyadmin:latest&
+
+docker run --rm -it --name phpfpm --net app -p 3001:80 -v ./src:/var/www/html:ro -e "WEBROOT=/var/www/html/public/" -e SKIP_COMPOSER=1 -e SKIP_CHMOD=1 -e SKIP_CHOWN=1 -v ./src/nginx.conf:/etc/nginx/sites-enabled/default.conf:ro richarvey/nginx-php-fpm:latest
