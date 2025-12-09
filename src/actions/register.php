2ab24007b315413fe4be914ab49c4ab3038559bc
adalbogant <louis.garlement@gmail.com>
2025-12-09 11:27:08 +0100

    Ajout pages de création de compte

--- src/actions/register.php
+++ src/actions/register.php
@@ -1,5 +1,82 @@
 <?php
 
+require_once __DIR__ . "/../template/layout.php";
 
+// Gère le POST /register avec nom/prénom/email/password/password2
 
-?>
+$name = $_POST["name"];
+$first_name = $_POST["first_name"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$password2 = $_POST["password2"];
+
+if(empty($email) || empty($password) || empty($name) || empty($first_name) || empty($password2)) {
+
+        render_page("register.php", ["title"=>"Finance App - Se connecter",
+        "error"=>"Veuillez entrer toutes les informations de connexion",
+        "email"=>$email, "name"=>$name, "first_name"=>$first_name]);
+
+        die();
+}
+
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+        render_page("register.php", ["title"=>"Finance App - Se connecter",
+        "error"=>"Adresse mail invalide",
+        "email"=>$email, "name"=>$name, "first_name"=>$first_name]);
+
+        die();
+}
+
+if(strlen($name) > 50) {
+
+        render_page("register.php", ["title"=>"Finance App - Se connecter",
+        "error"=>"La taille du nom doit être inférieure à 50 caractères",
+        "email"=>$email, "name"=>$name, "first_name"=>$first_name]);
+
+        die();
+}
+
+if(strlen($first_name) > 50) {
+
+        render_page("register.php", ["title"=>"Finance App - Se connecter",
+        "error"=>"La taille du prénom doit être inférieure à 50 caractères",
+        "email"=>$email, "name"=>$name, "first_name"=>$first_name]);
+
+        die();
+}
+
+if(strlen($email) > 100) {
+
+        render_page("register.php", ["title"=>"Finance App - Se connecter",
+        "error"=>"La taille du nom doit être inférieure à 50 caractères",
+        "email"=>$email, "name"=>$name, "first_name"=>$first_name]);
+
+        die();
+}
+
+if($password!=$password2) {
+
+        render_page("register.php", ["title"=>"Finance App - Se connecter",
+        "error"=>"Les mots de passes ne sont pas identiques",
+        "email"=>$email, "name"=>$name, "first_name"=>$first_name]);
+
+        die();
+}
+
+$auth = new Auth();
+
+if($auth->exists($email)) {
+    render_page("register.php", ["title"=>"Finance App - Se connecter", "error"=>"Inscription impossible - Cette adresse mail est déjà utilisée.",
+    "email"=>$email, "name"=>$name, "first_name"=>$first_name]);
+    die();
+}
+
+if(!$auth->register($name, $first_name, $email, $password)) {
+    render_page("register.php", ["title"=>"Finance App - Se connecter", "error"=>"Inscription impossible - Une erreur est survenue.",
+    "email"=>$email, "name"=>$name, "first_name"=>$first_name]);
+    die();
+}
+
+header("Location: /");
+die();
--- src/pages/register.php
+++ src/pages/register.php
@@ -1,14 +1,30 @@
-<h1>Entrez vos informations de connection</h1>
+<div class="login">
 
-<form action="" method="post" class="center-col">
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email" value="<?= @$email ?>" />
-    <label for="password">Mot de passe</label>
-    <input type="password" name="password" id="password" />
+    <h1>Entrez vos informations de connexion</h1>
 
-    <?php if(isset($error)) { ?>
-        <span style="color: red;"><?= $error ?></span>
-    <?php } ?>
+    <form action="" method="post" class="center-col">
+        <label for="name">Nom</label>
+        <input type="string" name="name" id="name" value="<?= @$name ?>" />
 
-    <input type="submit"/>
-</form>
+        <label for="first_name">Prénom</label>
+        <input type="string" name="first_name" id="first_name" value="<?= @$first_name ?>" />
+
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" value="<?= @$email ?>" />
+
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password" />
+
+        <label for="password2">Confirmez votre mot de passe</label>
+        <input type="password" name="password2" id="password2" />
+
+        <?php if (isset($error)) { ?>
+            <span style="color: red;"><?= $error ?></span>
+        <?php } ?>
+
+        <input type="submit" value="S'inscrire" />
+    </form>
+
+<a href="/login"> Revenir à la connexion </a>
+
+</div>
